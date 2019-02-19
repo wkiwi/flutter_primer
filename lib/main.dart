734e599c52414ac51529ff888264ccf480a5ec60
wkiwi <15619986615@163.com>
2019-02-19 11:35:27 +0800

    basic

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import './demo/listview_demo.dart';
 import './demo/hello_demo.dart';
 import './demo/drawer_demo.dart';
 import './demo/bottom_navigation_bar_demo.dart';
+import './demo/basic_demo.dart';
 
 void main(){
   runApp(
@@ -65,7 +66,8 @@ class Home extends StatelessWidget{
         children: <Widget>[
           // Icon(Icons.local_florist,size: 128.0, color: Colors.black12),
           ListViewDemo(),
-          Icon(Icons.change_history,size: 128.0, color: Colors.black12),
+          // Icon(Icons.change_history,size: 128.0, color: Colors.black12),
+          BasicDemo(),
           Icon(Icons.directions_bike,size: 128.0, color: Colors.black12),
         ],
       ),

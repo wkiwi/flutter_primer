010a12e3262bf6e93d49253bd5fae93794d8d020
wkiwi <15619986615@163.com>
2019-02-19 17:21:17 +0800

    view

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import './demo/drawer_demo.dart';
 import './demo/bottom_navigation_bar_demo.dart';
 import './demo/basic_demo.dart';
 import './demo/layout_demo.dart';
-
+import './demo/view_demo.dart';
 void main(){
   runApp(
     App()
@@ -33,7 +33,7 @@ class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
+      length: 4,
       child: Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
@@ -60,6 +60,7 @@ class Home extends StatelessWidget{
             Tab(icon:Icon(Icons.local_florist)),
             Tab(icon:Icon(Icons.change_history)),
             Tab(icon:Icon(Icons.directions_bike)),
+            Tab(icon:Icon(Icons.view_quilt)),
           ],
         ),
       ),
@@ -70,7 +71,9 @@ class Home extends StatelessWidget{
           // Icon(Icons.change_history,size: 128.0, color: Colors.black12),
           BasicDemo(),
           // Icon(Icons.directions_bike,size: 128.0, color: Colors.black12),
-          LayOutDemo()
+          LayOutDemo(),
+
+          ViewDemo(),
         ],
       ),
       drawer: DrawerDemo(),

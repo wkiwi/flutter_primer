f37c4409adc1664737312a9b3c16831651bdf945
wkiwi <15619986615@163.com>
2019-02-20 09:58:34 +0800

    路由

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import './demo/basic_demo.dart';
 import './demo/layout_demo.dart';
 import './demo/view_demo.dart';
 import './demo/sliver_demo.dart';
+import './demo/navigator_demo.dart';
 void main(){
   runApp(
     App()
@@ -18,7 +19,12 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:SliverDemo(),
+      // home:NavigatorDemo(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+        '/about':(context) =>Page(title: 'About')
+      },
       theme: ThemeData(
         primarySwatch: Colors.yellow,
         highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
